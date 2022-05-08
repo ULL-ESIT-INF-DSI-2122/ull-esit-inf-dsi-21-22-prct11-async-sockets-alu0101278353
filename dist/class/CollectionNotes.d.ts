55116cb41b4d4ec791b34322e827fa08e9486f6b
alu0101278353 <43884056+alu0101278353@users.noreply.github.com>
2022-05-08 23:20:26 +0200

    delete

--- dist/class/CollectionNotes.d.ts
+++
@@ -1,7 +0,0 @@
-import { Note } from './Notes';
-export declare class CollectionNotes {
-    protected ListNotes: Set<Note>;
-    addNote(note: Note): void;
-    removeNote(note: Note): void;
-    getListNotes(): Set<Note>;
-}
